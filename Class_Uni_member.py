420b7e85c3a68d5a4dea66ee61670c798c3f5a08
Reptilius <blockgen@yandex.ru>
2023-12-12 22:54:36 +0300

    Add Classes, Genomics, Exercises

--- Class_Uni_member.py
+++ Class_Uni_member.py
@@ -1,4 +1,4 @@
-class Uni_member:
+class UniMember:
     num_of_members = 0
     SEMESTER_BONUS = 1.15
 
@@ -11,7 +11,7 @@ class Uni_member:
         return f'{self.first_name} {self.last_name}'
 
 
-class Assistant(Uni_member):
+class Assistant(UniMember):
     def __init__(self, first_name, last_name, salary, courses):
         super().__init__(first_name, last_name)
         self.salary = salary
@@ -23,23 +23,23 @@ class Assistant(Uni_member):
     def __repr__(self):
         return f"Assistant('{self.first_name}', '{self.last_name}', {self.salary}, '{self.courses}')"
 
-class Student(Uni_member):
+
+class Student(UniMember):
     def __init__(self, first_name, last_name, scolarship):
         super().__init__(first_name, last_name)
         self.scolarship = scolarship
 
     @property
     def change_email(self):
-        self.email =  self.first_name + '.' + self.last_name + '@uni.edu'
+        self.email = self.first_name + '.' + self.last_name + '@uni.edu'
 
     def __str__(self):
         return f'Hello! My name is {self.first_name} {self.last_name}, give my {self.scolarship} dollars, please!'
 
-class Professor(Uni_member):
-    def __init__(self, first_name, last_name, salary, assistants = None):
-        #super().__init__(first_name, last_name)
-        self.first_name = first_name
-        self.last_name = last_name
+
+class Professor(UniMember):
+    def __init__(self, first_name, last_name, salary, assistants=None):
+        super().__init__(first_name, last_name)
         self.salary = salary
         if assistants is None:
             self.assistants = []
@@ -60,3 +60,6 @@ class Professor(Uni_member):
     def print_assist(self):
         for assist in self.assistants:
             print('My assistant is ->', assist.full_name())
+
+    def assist_quantaty(self):
+        return f'Quantaty of assistants is: {len(self.assistants)}'
--- Test_OOP.py
+++ Test_OOP.py
@@ -1,13 +1,12 @@
 from Class_Uni_member import *
 
-student1 = Uni_member('Ivan', 'Petrov')
-student2 = Uni_member('Alex', 'Popov')
-
+student1 = UniMember('Ivan', 'Petrov')
+student2 = UniMember('Alex', 'Popov')
 
 ast1 = Assistant('Petr', 'Volkov', 15500, 'geometry')
 ast2 = Assistant('Maya', 'Lomova', 12500, 'economy')
 
-student3 = Student('Vin', 'Diesel',2500)
+student3 = Student('Vin', 'Diesel', 2500)
 student4 = Student('Niki', 'Lauda', 7500)
 student5 = Student('Jack', 'White', 9500)
 
@@ -17,3 +16,13 @@ print(student5.email)
 student5.last_name = 'Black'
 student5.change_email
 print(student5.email)
+
+stud_1 = UniMember('Ivan', 'Ivanov')
+
+print(stud_1.full_name())
+
+proff_1.add_assistant(student1)
+proff_1.add_assistant(ast1)
+proff_1.print_assist()
+print(proff_1.assist_quantaty())
+
