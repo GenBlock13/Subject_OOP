09ce0d9b1a8b8d831c47bcb434419bfafaea0ab2
Reptilius <blockgen@yandex.ru>
2023-12-15 22:52:32 +0300

    Add tkinter

---
+++ TKinter/tkinter_1.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+window = tk.Tk(
+    screenName="main",
+
+)
+
+greeting = tk.Label(
+    text="Start now!",
+    width=20,
+    height=7,
+    background="green",
+    font="blue"
+)
+
+label = tk.Label(
+    text="Привет, Tkinter!",
+    fg="white",
+    bg="black",
+    width=60,
+    height=40
+)
+
+button_close = tk.Button(
+    text="Close",
+    height=3,
+    width=6,
+    background="cyan",
+    command=window.destroy
+
+)
+
+label.pack()
+button_close.pack(
+    fill=tk.Y, side=tk.LEFT
+)
+greeting.pack(fill = tk.Y, side = tk.LEFT)
+window.mainloop()
