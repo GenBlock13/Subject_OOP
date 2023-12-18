119e546b40187a6481133a437baa2e8d937b5cfd
Reptilius <blockgen@yandex.ru>
2023-12-18 22:41:19 +0300

    Update func_visibility.py
    __dict__.get()

--- Functions/func_visibility.py
+++ Functions/func_visibility.py
@@ -1,17 +1,15 @@
-
 marks = [4, 5, 3, 2, 7]
 
+
 def increase(marks):
-    marks = [1,2,3,4]
+    marks = [1, 2, 3, 4]
     print(*marks)
+
 
 def new_marks(marks):
     marks += [777]
     print(increase(marks))
 
-increase(marks)
-
-new_marks(marks)
 
 def gcd(x, y):
     gcd = min(x, y)
@@ -20,8 +18,10 @@ def gcd(x, y):
             gcd = i
             return gcd
 
+
 num_1 = gcd(1440, 680)
 print(num_1)
+
 
 def number_length(number):
     number = abs(number)
@@ -32,7 +32,9 @@ def number_length(number):
         len += 1
     return len
 
+
 print(number_length(-88887))
+
 
 def month(number, lang):
     months = {1: ['January', 'Январь'], 2: ['February', 'Февраль'], 3: ["March", "Март"],
@@ -44,8 +46,32 @@ def month(number, lang):
     else:
         return months[number][1]
 
-months = month(8, "ru")
 
+months = month(8, "ru")
 print(months)
 
 
+def split_numbers(list):
+    answer = []
+    for i in list:
+        if i.isdigit():
+            answer.append(i)
+        elif i == " ":
+            pass
+        else:
+            answer.append(i)
+    return tuple(answer)
+
+def modern_print(string):
+    if modern_print.__dict__.get('printed_strings') is None:
+        modern_print.printed_strings = set()
+    if string not in modern_print.printed_strings:
+        print(string)
+        modern_print.printed_strings.add(string)
+
+
+modern_print("hello")
+modern_print("hello")
+modern_print("Jam")
+modern_print("Kate")
+modern_print("Kate")
